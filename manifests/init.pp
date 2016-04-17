6c89882f1186ae14558cc505cd93cab2b647e8e1
Markus Benning <ich@markusbenning.de>
2016-04-17 19:33:31 +0200

    add description and usage of module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,28 @@
 # == Class: docker_bamberg
 #
-# Full description of class docker_bamberg here.
+# This puppet module contains an example configuration of a web blog
+# for a presentation at the Docker Bamberg meetup.
 #
 # === Parameters
 #
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# no parameters.
 #
 # === Variables
 #
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# no variables.
 #
 # === Examples
 #
 #  class { 'docker_bamberg':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
 #  }
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Markus Benning <ich@markusbenning.de>
 #
 # === Copyright
 #
-# Copyright 2016 Your name here, unless otherwise noted.
+# Copyright 2016 Markus Benning
 #
 class docker_bamberg {
-
-
 }
